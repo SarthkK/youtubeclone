2f27eccaca284b69dd325cf07a84e0ab77da345b
Sarthk Kharwal <sarthkkharwal@gmail.com>
2025-06-06 11:38:33 +0530

    categories completed

--- src/modules/categories/server/procedures.ts
+++ src/modules/categories/server/procedures.ts
@@ -5,7 +5,6 @@ import { TRPCError } from "@trpc/server";
 
 export const categoriesRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
-    throw new TRPCError({ code: "BAD_REQUEST" });
     const data = await db.select().from(categories);
     return data;
   }),
--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -3,22 +3,44 @@
 import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
+import { FilterCarousel } from "@/components/filter-carousel";
+import { useRouter } from "next/navigation";
 
 interface CategoriesSectionProps {
   categoryId?: string;
 }
 export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <ErrorBoundary fallback={<p>Error...</p>}>
+    <ErrorBoundary fallback={<p>Error...</p>}>
+      <Suspense fallback={<CategoriesSkeleton />}>
         <CategoriesSectionSuspense categoryId={categoryId} />
-      </ErrorBoundary>
-    </Suspense>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
+
+const CategoriesSkeleton = () => {
+  return <FilterCarousel isLoading data={[]} onSelect={() => {}} />;
+};
+
 export const CategoriesSectionSuspense = ({
   categoryId,
 }: CategoriesSectionProps) => {
+  const router = useRouter();
   const [categories] = trpc.categories.getMany.useSuspenseQuery();
-  return <div>{JSON.stringify(categories)}</div>;
+
+  const data = categories.map(({ name, id }) => ({ value: id, label: name }));
+
+  const onSelect = (value: string | null) => {
+    const url = new URL(window.location.href);
+
+    if (value) {
+      url.searchParams.set("categoryId", value);
+    } else {
+      url.searchParams.delete("categoryId");
+    }
+
+    router.push(url.toString());
+  };
+  return <FilterCarousel onSelect={onSelect} value={categoryId} data={data} />;
 };
